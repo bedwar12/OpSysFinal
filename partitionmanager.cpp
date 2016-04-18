dfcca6e9573b4ce26590bcf35a83369df4b6de21
Michael McCormick <mmccorm4@gmail.com>
2016-04-18 15:14:18 -0600

    Completed partitionManager
    Moving on to the File System

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -11,13 +11,33 @@ PartitionManager::PartitionManager(DiskManager *dm, char partitionname, int part
   myDM = dm;
   myPartitionName = partitionname;
   myPartitionSize = myDM->getPartitionSize(myPartitionName);
+	freeSpace = new BitVector(partitionsize);
+	char buffer[64];
+	for(int i = 0; i < 64; i++){
+		buffer[i] = 'c';
+	}
 
-  /* If needed, initialize bit vector to keep track of free and allocted
-     blocks in this partition */
+  /* If needed, initialize bit vector to keep track of free and       allocted blocks in this partition */
+  myDM -> readDiskBlock(myPartitionName, 0, buffer);
+  if(buffer[0] != 'c')
+  {
+  	// create bitvector from stored data
+  	freeSpace -> setBitVector((insigned in*) buffer);
+  }
+  else
+  {
+  	//BitVector is all 0's except for blocks 0 and 1
+  	freeSpace -> setBit(0);
+  	freeSpace -> setBit(1);
+  	freeSpace -> getBitVector((unsigned int *) buffer);
+  	int r;
+  	r = myDM -> writeDiskBlock(myPartitionName, 0, buffer);
+  	}
 }
 
 PartitionManager::~PartitionManager()
 {
+  delete [] freeSpace;
 }
 
 /*
@@ -34,11 +54,11 @@ int PartitionManager::getFreeDiskBlock()
   	/* write the code for allocating a partition block */
 	for(int index = 0; index < myPartitionSize; index++)
 	{
-		if(myBV->testBit(index) == 0)
+		if(freeSpace->testBit(index) == 0)
 		{
 			// update bitvector
-			myBV->setBit(index);
-			myBV->getBitVector((unsigned int *) buffer);
+			freeSpace->setBit(index);
+			freeSpace->getBitVector((unsigned int *) buffer);
 			int r;
 			r = myDM->writeDiskBlock(myPartitionName, 0, buffer);
 			return index;
@@ -53,6 +73,30 @@ int PartitionManager::getFreeDiskBlock()
 int PartitionManager::returnDiskBlock(int blknum)
 {
   /* write the code for deallocating a partition block */
+  // if blknum is out of bounds, fail with -1
+  if(blknum < 0 || blknum > myPartitionSize)
+    return -1;
+  // else if block is deallocated, do nothing, return 0 for success
+  else if(freeSpace->testBit(blknum) == 0)
+    return 0;
+  // else, free the allocated block
+  else{
+    // create "blank" buffer filled with 'c'
+    char blankBuffer[64];
+    for (int i = 0; i < 64; i++){
+        blankBuffer[i] = 'c';
+    }
+    // write blank buffer to disk
+    myDM->writeDiskBlock(myPartitionName,blknum, buffer);
+    // reset bit at location blknum
+    freeSpace->resetBit(blknum);
+    // get updated bitvector
+    freeSpace->getBitVector((unsigned int*) buffer);
+    // write updated bitVector to loc 0 of this partition
+    myDM->writeDiskBlock(myPartitionName,0,buffer);
+
+    return 0;
+  }
 }
 
 
