8f26a41b83fbde2fada6edead12982065a6d55ba
Michael McCormick <mmccorm4@gmail.com>
2016-04-22 15:08:36 -0600

    Finished Lock and Unlock File

--- filesystem.cpp
+++ filesystem.cpp
@@ -27,10 +27,60 @@ int FileSystem::createDirectory(char *dirname, int dnameLen)
 }
 int FileSystem::lockFile(char *filename, int fnameLen)
 {
+  int location, par, child;
 
+	if(!validName(filename, fnameLen))
+	{
+		return -4;
+	}
+
+	// check if file exists
+	location = findBlockNum( filename, par, child );
+	if( location == (-1) )
+	{
+		// file does not exist
+		return (-2);
+	}
+
+	int lock;
+	if( isLocked( filename ) )
+	{
+		return( -1 ); // already locked
+	}
+
+	//
+	char buffer[65];
+
+	// write new lock value to disk, and return it
+	lock = rand();
+	myLockTable[filename] = lock;
+	return lock;
 }
+
 int FileSystem::unlockFile(char *filename, int fnameLen, int lockId)
 {
+  int lock;
+
+	if(!validName(filename, fnameLen))
+	{
+		return -4;
+	}
+	if(isLocked(filename))
+	{
+		if(myLockTable[filename] == lockId)
+		{
+			myLockTable.erase(filename);
+			return 0;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	else
+	{
+		return -2;
+	}
 
 }
 int FileSystem::deleteFile(char *filename, int fnameLen)
